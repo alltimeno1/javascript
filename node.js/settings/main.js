89764ae7be17f9c42fc88dbf264b02748a48758f
SungJun Kim <98739079+alltimeno1@users.noreply.github.com>
2022-02-12 19:30:45 +0900

    eslintrc

--- node.js/settings/main.js
+++ node.js/settings/main.js
@@ -1,3 +1,25 @@
-// Formatting, Linting
+// @ts-check
+// Formatting, Linting, Type Checking
 // Formatting : prettier
+// Linting : ESLint
+// Type checking : TypeScript
+
 console.log('Hello, world!')
+
+const x = 1
+console.log(x)
+
+const string = 'Hello'
+const result = Math.log(string)
+console.log(result)
+
+const http = require('http')
+
+const server = http.createServer((req, res) => {
+  res.statusCode = res.end('Hello!')
+})
+
+const PORT = 4000
+server.listen(PORT, () => {
+  console.log(`The server is listening at port: ${PORT}.`)
+})
