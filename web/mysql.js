72a6e9e5d1bb8baf179fe70af2caa2f51bd50853
Sungjun Kim <hzcd108@gmail.com>
2022-03-14 17:52:43 +0900

    crud with mysql

---
+++ web/mysql.js
@@ -0,0 +1,83 @@
+const express = require('express');
+
+const app = express();
+
+const db = require('./models');
+
+const { Member } = db;
+
+// 미들웨어
+app.use(express.json());
+
+// GET
+app.get('/api/members', async (req, res) => {
+  const { team } = req.query;
+  if (team) {
+    const teamMembers = await Member.findAll({ where: { team }});
+    res.send(teamMembers);
+  } else {
+    const members = await Member.findAll();
+    res.send(members);
+  }
+});
+
+app.get('/api/members/:id', async (req, res) => {
+  const { id } = req.params;
+  const member = await Member.findOne({ where: { id }});
+
+  if (member) {
+    res.send(member);
+  } else {
+    res.status(404).send({ message: 'There is no member with the id' });
+  }
+});
+
+// POST
+app.post('/api/members', async (req, res) => {
+  const newMember = req.body;
+  // const member = Member.build(newMember);
+  // await member.save();
+  const member = await Member.create(newMember);
+  res.send(member);
+});
+
+// PUT
+app.put('/api/members/:id', async (req, res) => {
+  const { id } = req.params;
+  const newInfo = req.body;
+  const result = await Member.update(newInfo, { where: { id }});
+  if (result[0]) {
+    res.send({ message: `${result[0]} row(s) affected` });
+  } else {
+    res.status(404).send({ message: 'There is no member with the id' });
+  }
+})
+
+// PUT v2
+// app.put('/api/members/:id', async (req, res) => {
+//   const { id } = req.params;
+//   const newInfo = req.body;
+//   const member = await Member.findOne({ where: { id } });
+//   if (member) {
+//     Object.keys(newInfo).forEach(prop => member[prop] = newInfo[prop]);
+//     await member.save();
+//     res.send(member);
+//   } else {
+//     res.status(404).send({ message: 'There is no member with the id' });
+//   }
+// });
+
+// DELETE
+app.delete('/api/members/:id', async (req, res) => {
+  const { id } = req.params;
+  const deletedCount = await Member.destroy({ where: { id } });
+  if (deletedCount) {
+    res.send({ message: `${deletedCount} row(s) deleted` });
+  } else {
+    res.status(404).send({ message: 'There is no member with the id' });
+  }
+})
+
+app.listen(3000, () => {
+  console.log('Server is listening...');
+});
