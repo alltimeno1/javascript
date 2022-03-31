c071cf44c213ee7e08e2a62d44cb459f92606a72
SungJun Kim <98739079+alltimeno1@users.noreply.github.com>
2022-03-31 18:50:45 +0900

    Update and rename algorithm_quiz to algorithm_quiz.js

--- basics/algorithm_quiz.js
+++ basics/algorithm_quiz.js
@@ -37,3 +37,38 @@ function solution(priorities, location) {
     
     return answer;
 }
+
+// https://programmers.co.kr/learn/courses/30/lessons/42862#
+
+function solution(n, lost, reserve) {
+    let list = [...new Array(n)].map(n => 1);
+    
+    lost.forEach(id => list[id - 1] -= 1);
+    reserve.forEach(id => list[id - 1] += 1);
+    
+    for(let i = 0; i < list.length; i++) {
+        if (list[i] == 2 & list[i - 1] == 0) {
+            list[i] -= 1;
+            list[i - 1] += 1;
+        } else if (list[i] == 2 & list[i + 1] == 0) {
+            list[i] -= 1;
+            list[i + 1] += 1;
+        }
+    }
+    
+    return list.filter(n => n).length
+}
+
+// https://programmers.co.kr/learn/courses/30/lessons/42842
+
+function solution(brown, yellow) {
+    const total = brown + yellow;
+    let list = [];
+    
+    for (let y = 3; y <= Math.sqrt(total); y++) {
+        const x = total / y
+        if (!(total % x) && (x - 2) * (y - 2) == yellow) {
+            return [x, y];
+        }
+    }
+}
