3f2c1fbd28ab2e123ce13f01e014fc16d70c24f3
SungJun Kim <98739079+alltimeno1@users.noreply.github.com>
2022-04-12 08:43:56 +0900

    Update mongo.js

--- db/mongo.js
+++ db/mongo.js
@@ -1,6 +1,6 @@
 const { MongoClient } = require('mongodb')
 
-const uri = `mongodb+srv://ksj:mongo108@cluster0.nprnd.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`
+const uri = `mongodb+srv://${process.env.MONGO_USER}:${process.env.MONGO_PASSWORD}@cluster0.nprnd.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`
 const client = new MongoClient(uri, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
